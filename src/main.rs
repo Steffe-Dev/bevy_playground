387389c88b65f51c54c3f561287cecbb49c60925
Frans Stefanus Bothma <fstefanusbothma@gmail.com>
2025-05-06 21:55:05 +0200

    Finish quick start

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use bevy::prelude::*;
 
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, (update_people, greet_people).chain());
+    }
+}
+
 #[derive(Component)]
 struct Person;
 
@@ -12,9 +22,13 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("Hello, {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused our timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello, {}!", name.0);
+        }
     }
 }
 
@@ -27,14 +41,12 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
     }
 }
 
-fn hello_world() {
-    println!("Hello, world!");
-}
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        // hello_world can run in parallel with the chained systems
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .add_plugins(MinimalPlugins)
+        .add_plugins(HelloPlugin)
         .run();
 }
